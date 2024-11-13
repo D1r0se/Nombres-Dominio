006c591a46741f3b33d4b282d1bc335e4d6e6db4
D1r0se <154914732+D1r0se@users.noreply.github.com>
2024-11-13 17:55:45 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "hola"];
+  let adj = ["great", "big", "mundo"];
+  let noun = ["jogger", "racoon", "soyJesus"];
+  let extensions = [".com", ".net", ".es"];
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++)
+        for (let d = 0; d < noun.length; d++) {
+          let result = pronoun[a] + adj[b] + noun[c] + extensions[d];
+          console.log(result);
+        }
+    }
+  }
 };
